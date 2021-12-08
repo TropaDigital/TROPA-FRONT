1166627f17aa427dd1d277399254445c32f7e90d
Fino59 <slipdan@hotmail.com>
2021-12-08 17:56:23 -0300

    Update Sistemas page with Modal

--- src/pages/sistemas.tsx
+++ src/pages/sistemas.tsx
@@ -9,13 +9,25 @@ import Contact from '../components/Contact';
 import Footer from '../components/Layout/Footer';
 import { BannerSystem, LanguagesContainer, SystemContainer } from '../assets/styles/SystemStyles';
 import { ContainerCenter } from '../components/Layout/styles';
-import WhatWeDo from '../components/WeDo';
-import { IconCash, IconChatbot, IconGlobal, IconMessage } from '../components/Svg';
+import { IconAstroFill, IconCash, IconChatbot, IconClose, IconGlobal, IconMessage } from '../components/Svg';
+import ModalDefault from '../components/Modal';
+import { useState } from 'react';
+import Chatbot from '../components/Chatbot/Chatbot';
+import { WeDoContainer } from '../components/WeDo/styles';
+import { Chat, ChatbotContainer, DivChat } from '../components/Chatbot/styles';
+import { CardContainer, MessageContainer, Messages } from '../components/Mensagerias/styles';
 
 const Sistemas: NextPage = () => {
     const router = useRouter();
+    
+    const [selected, setSelected] = useState<any>(null);
+
+    const [modalBot, setModalBot] = useState<boolean>(false);
+    const [modalMsg, setModalMsg] = useState<boolean>(false);
+    const [modalCheck, setModalCheck] = useState<boolean>(false);
 
     return (
+    <>
         <Layout>
             <SystemContainer>
 
@@ -71,14 +83,92 @@ const Sistemas: NextPage = () => {
                     </BannerSystem>
                 </Header>
 
-                <WhatWeDo
-                    title="O Que Fazemos?"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque suscipit nulla non vehicula."
-                    icon={<IconGlobal className="effect-stroke grey" />}
-                    items={[
+                <WeDoContainer>
+                    <ContainerCenter className="center">
+                        <div className="left">
+                            <Animate effect="fadeInLeft" startAnimation={200}>
+                                <div className="title">
+                                    <h3>O Que Fazemos?</h3>
+                                    <IconGlobal className="effect-stroke grey" />
+                                </div>
+                            </Animate>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque suscipit nulla non vehicula.</p>
+                        </div>
+                        
+                        <div className="stages">
+
+                                <Animate effect="zoomIn" startAnimation={200}>
+                                    <div onClick={() => setModalBot(true)}>
+                                        <div
+                                            className={`item ${
+                                                selected === 'bot' ? 'active bot' : 'inactive bot'
+                                            }`}                                
+                                            onClick={() =>
+                                                selected === 'bot'
+                                                    ? setSelected(null)
+                                                    : setSelected('bot')
+                                            }
+                                        >
+                                            <div className="title" >
+                                                <div><IconChatbot /></div>
+                                                <span>Chatbot</span>                                    
+                                            </div>
+                                            <div className="description">
+                                                SIMULE AGORA MESMO.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Animate>
+
+                                <Animate effect="zoomIn" startAnimation={200}>
+                                    <div onClick={() => setModalCheck(true)}>
+                                        <div
+                                            className={`item ${
+                                                selected === "cash" ? 'active cash' : 'inactive cash'
+                                            }`}                                
+                                            onClick={() =>
+                                                selected === "cash"
+                                                    ? setSelected(null)
+                                                    : setSelected('cash')
+                                            }
+                                        >
+                                            <div className="title">
+                                                <div><IconCash /></div>
+                                                <span>checkout transparente</span>                                    
+                                            </div>
+                                            <div className="description">
+                                                SIMULE AGORA MESMO.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Animate>
+
+                                <Animate effect="zoomIn" startAnimation={200}>
+                                    <div onClick={() => setModalMsg(true)}>
+                                        <div
+                                            className={`item ${
+                                                selected === "msg" ? 'active msg' : 'inactive msg'
+                                            }`}                                
+                                            onClick={() =>
+                                                selected === "msg"
+                                                    ? setSelected(null)
+                                                    : setSelected('msg')
+                                            }
+                                        >
+                                            <div className="title">
+                                                <div><IconMessage /></div>
+                                                <span>Mensagerias</span>                                    
+                                            </div>
+                                            <div className="description">
+                                                SIMULE AGORA MESMO.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Animate>
                             
-                    ]}
-                />
+                        </div>
+                    </ContainerCenter>
+                </WeDoContainer>
 
                 <LanguagesContainer>
                     <ContainerCenter className="center">
@@ -108,8 +198,129 @@ const Sistemas: NextPage = () => {
                 <Contact />
                 
                 <Footer />
+
             </SystemContainer>
+
+            {modalBot && (
+                <ModalDefault
+                    id="overlayModal"
+                    onClose={() => setModalBot(!modalBot)}
+                    openModal={modalBot}                
+                >
+                    <ChatbotContainer>
+                        <DivChat>
+                            
+                            <div>
+                                <h1>Com quem quer falar?</h1>
+                            </div>
+                            <div className="buttons">
+                                <button>Astrobot</button>
+                                <button>Einsteinbot</button>
+                                <button>Outrobot</button>
+                            </div>
+
+                            <Chat>
+                                <div className="bot">
+                                    <div className="icon">
+                                        <IconChatbot />
+                                    </div>
+                                    <div className="chat">
+                                        <p>Astrobot</p>
+                                        <span>Oi, eu sou o BOT, como posso te ajudar?</span>
+                                    </div>
+                                </div>
+
+                                <div className="user">
+                                    <div className="chat">
+                                        <p>Você</p>
+                                        <span>Eu gostaria de testar um sistema</span>
+                                    </div>
+                                    <div className="astro">
+                                        <IconAstroFill />
+                                    </div>
+                                </div>
+
+                                <div className="send">
+                                    <input type="text" placeholder="Digite sua mensagem" />
+                                    <button>
+                                        <IconMessage /> 
+                                    </button>
+                                </div>
+                            </Chat>
+
+                            <div className="personalized">
+                                <p>Quer um </p>
+                                <p className="primary"> bot personalizado?</p>
+                            </div>
+
+                        </DivChat>           
+
+                        <button className="close" 
+                            onClick={() => setModalBot(false)}
+                        >
+                            <IconClose />
+                        </button>
+                    </ChatbotContainer>    
+                    
+                </ModalDefault>
+            )}
+
+            { modalMsg && (
+                <ModalDefault
+                    id="overlayModal"
+                    onClose={() => setModalMsg(!modalMsg)}
+                    openModal={modalBot}                
+                >
+                    <MessageContainer>
+
+                        <h3>Mensagerias</h3>
+
+                        <CardContainer>
+            
+                            <div className="receiver">
+                                <h3>Por onde quer receber?</h3>
+                                
+                                <div className="buttons">
+                                    <button>Whatsapp</button>
+                                    <button>SMS</button>
+                                </div>
+                            </div>
+            
+                            <Messages>
+                                <div className="msg">
+                                    <h3>Selecione uma mensagem:</h3>
+                                    <button>Olá, eu sou uma mensagem automática</button>
+                                    <button>Olá, eu sou uma mensagem automática</button>
+                                    <button>Olá, eu sou uma mensagem automática</button>
+                                </div>
+            
+                                <div className="edit">
+                                    <p>Edite a mensagem</p>
+                                    <input type="text" placeholder="Lorem Ipsum Stanley Ipkis" />
+                                </div>
+                            </Messages>
+            
+                            <button>Enviar mensagem</button>
+            
+                        </CardContainer>    
+
+                        <div className="personalized">
+                            <p>Quer um </p>
+                            <p className="primary"> bot personalizado?</p>
+                        </div>
+            
+                    </MessageContainer>
+                </ModalDefault>
+            )}
+
+            {/* { modalCheck && (
+
+            )} */}
+            
+
         </Layout>
+
+    </>
     )
 };
 
