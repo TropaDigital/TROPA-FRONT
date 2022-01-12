7df3327da181daae27e999baac173bebfcf461f7
Fino59 <slipdan@hotmail.com>
2022-01-12 17:57:33 -0300

    Update with Loader component

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -5,6 +5,7 @@ import Animate from '../Animation/Animate';
 import { ContainerCenter } from '../Layout/styles';
 import { IconArrowLeft, IconArrowRight, IconFlag } from '../Svg';
 import { SocialContainer, SocialContainerCarousel } from './styles';
+import PulseLoader from "react-spinners/PulseLoader";
 
 interface IProps {
     itemsToShow?: number;
@@ -12,25 +13,7 @@ interface IProps {
 
 const Social: React.FC<IProps> = ({ itemsToShow = 3 }) => {
     const [socialMedia, setSocialMedia] = useState([])
-    // const [data, setData] = useState([
-    //     {
-    //         image: 'http://instagram.fcgh17-1.fna.fbcdn.net/v/t51.2885-15/e35/96286641_149876740000698_1508641614209261210_n.jpg?_nc_ht=instagram.fcgh17-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=-2YOVlhDWiMAX9R9JLT&tn=i2qYe6JJQZg2wtYV&edm=AP_V10EBAAAA&ccb=7-4&oh=963c2d8013a5e116e26f3523fa737010&oe=6195F977&_nc_sid=4f375e',
-    //         text: '🌹',
-    //     },
-    //     {
-    //         image: 'https://instagram.fcgh17-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/95493455_1087465958313213_1744366779184380857_n.jpg?_nc_ht=instagram.fcgh17-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=tRstQMXyj8QAX9FHO4y&edm=AP_V10EBAAAA&ccb=7-4&oh=b151a51122dd4e8aa81451bc14fd9cc9&oe=619494A3&_nc_sid=4f375e',
-    //         text: 'Estamos aqui para te ajudar!🚀 #sites #sitesresponsivos #seo #ecommerce #apps #midiadigital #plataforma #desenvolvimento #programação #fiqueemcasa #quarentena',
-    //     },
-    //     {
-    //         image: 'https://instagram.fcgh17-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/91260512_891668344625552_8816620437755387538_n.jpg?_nc_ht=instagram.fcgh17-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=yfF02m8NvqoAX8W_aNZ&edm=AP_V10EBAAAA&ccb=7-4&oh=b1b63ee869be9d4d37a798ed490d3095&oe=61965584&_nc_sid=4f375e',
-    //         text: 'A Internet oferece um mundo de possibilidades e conte conosco para tirar seus planos do papel e levar ao mundo digital! 💻 Juntos sairemos dessa! 😉🙏#ficaemcasa',
-    //     },
-    //     {
-    //         image: 'https://instagram.fcgh17-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/91136567_686247741914436_862729972517005629_n.jpg?_nc_ht=instagram.fcgh17-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=sJZjHYfkfsMAX9f4wrc&edm=AP_V10EBAAAA&ccb=7-4&oh=d4d6f8355915e29eb75a146cf0735a45&oe=619544D1&_nc_sid=4f375e',
-    //         text: 'Diante dessa quarentena, a internet está sendo nossa grande aliada, né?! Estamos conseguindo nos comunicar com as pessoas que estamos com saudades, as aulas/cursos estão podendo ser assistidos, as vendas sendo realizadas, as informações podendo ser conferidas, enfim...mesmo longe um dos outros, ela está nos unindo! Como está sendo sua quarentena, conte para nós!? Ah....e lembrando que ficamos à disposição para te ajudar nesse mundo digital e não parar o seu ramo profissional, hein! E tomem cuidado com as Fakenews! 😉 https://tropa.digital/ contato@tropa.digital 11-96368-6361 #tropadigital #sites #desenvolvimentodesites #plataformas #ecommerce #digital #apps #coronavirus #quarentena #covid19 #conscientizacao',
-    //     },
-    // ]);    
-
+    
     const breakpoints = ([
         { width: 1, itemsToShow: 1 },
         { width: 450, itemsToShow: 2, itemsToScroll: 2, pagination: false },
@@ -79,26 +62,38 @@ const Social: React.FC<IProps> = ({ itemsToShow = 3 }) => {
                             ref={slider}
                             breakPoints={breakpoints}
                         >
-                            {socialMedia.map((row: any, key: any) => (
-                                <div className="card" key={key}>
-                                    <div
-                                        className="image"
-                                        style={{
-                                            backgroundImage: 'url('+row.media_url+')',
-                                        }}
-                                    />
-                                    <div className="text">
-                                        <a
-                                            href={row.permalink}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            @tropadigitalagencia
-                                        </a>
-                                        <p>{row.caption}</p>
+                            {   socialMedia.length > 0 
+                                ?
+                                socialMedia.map((row: any, key: any) => (
+                                    <div className="card" key={key}>
+                                        <div
+                                            className="image"
+                                            style={{
+                                                backgroundImage: 'url('+row.media_url+')',
+                                            }}
+                                        />
+                                        <div className="text">
+                                            <a
+                                                href={row.permalink}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                @tropadigitalagencia
+                                            </a>
+                                            <p>{row.caption}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))
+                                :
+                                [1, 2, 3].map(() => (
+                                    <div className='loader'>
+                                        <PulseLoader 
+                                            color="#cc6138"
+                                            size={30} 
+                                        />
+                                    </div>
+                                ))   
+                            }
                         </Carousel>
                     </Animate>
                 </SocialContainerCarousel>
